7c800cecc366f3129c47bf11837cd001666ed9f8
AD1121 <sinagh30@gmail.com>
2023-11-04 17:56:22 +0100

    Mongodb Added

--- src/server.js
+++ src/server.js
@@ -4,12 +4,33 @@ const jwt = require("jsonwebtoken");
 const dataRoutes = require("./routes/data_routes");
 
 const connectDB = require("./config/db");
-const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
 
 const app = express();
+
+// Adding helmet for security
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "trusted-scripts.com"],
+        styleSrc: ["style.com"],
+      },
+    },
+    noCache: true, // Disable client-side caching
+  })
+);
+
+// Enable/disable specific headers
+app.use(helmet.frameguard({ action: "deny" }));
+
+// Connecting to Mongodb
+connectDB();
+
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // JWT Authentication Middleware
 app.use((req, res, next) => {

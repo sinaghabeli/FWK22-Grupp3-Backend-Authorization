50718c13aab7861806aca45acdaff760c8bbac29
AD1121 <sinagh30@gmail.com>
2023-11-04 17:44:29 +0100

    Files structure fixed

--- src/service.js
+++ src/service.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const app = require("./server.js");
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`http server listening on port ${PORT}`);
